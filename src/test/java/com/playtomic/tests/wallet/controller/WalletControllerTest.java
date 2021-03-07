f60565a34a26637975a1c06cb7e86add70b50e5a
Victor Espinosa Mateu <vicesmat@gmail.com>
2021-03-07 19:25:33 +0100

    added profiling to controller test

--- src/test/java/com/playtomic/tests/wallet/controller/WalletControllerTest.java
+++ src/test/java/com/playtomic/tests/wallet/controller/WalletControllerTest.java
@@ -9,21 +9,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.math.BigDecimal;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.playtomic.tests.wallet.api.WalletController;
+import com.playtomic.tests.wallet.WalletApplicationIT;
 import com.playtomic.tests.wallet.api.dto.WalletDto;
 import com.playtomic.tests.wallet.service.WalletService;
 import com.playtomic.tests.wallet.service.exception.NotFoundException;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(WalletController.class)
-public class WalletControllerTest {
+@AutoConfigureMockMvc
+public class WalletControllerTest extends WalletApplicationIT {
 	
 	@Autowired
 	private MockMvc mockMvc;
